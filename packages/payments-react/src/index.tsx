2eac1d1c29a2093413d38b8b6aad5d919b1c0738
Nestor Cortina <nexckycort@gmail.com>
2025-12-17 21:24:12 -0500

    feat: update package.json scripts for building and enhance index.tsx exports

--- packages/payments-react/src/index.tsx
+++ packages/payments-react/src/index.tsx
@@ -3,6 +3,5 @@ export type {
   CheckoutConfig,
   PaymentSubmitPayload,
 } from '@bloque/payments-elements';
-export { PAYMENT_METHODS } from '@bloque/payments-elements';
 export type { BloqueCheckoutProps } from './bloque-checkout';
 export { BloqueCheckout } from './bloque-checkout';
